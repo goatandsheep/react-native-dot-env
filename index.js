aad86d270877ec0369d2b24009eac7694c71103c
Florian Bernard <florian@Florians-MacBook-Pro.local>
2020-03-26 17:56:38 +0100

    update dependency

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-var path = require('path');
+var path = require("path");
 
 module.exports = () => ({
   plugins: [
-    [require('babel-plugin-dotenv'), {
-      replacedModuleName: 'react-native-dotenv',
-      configDir: path.resolve(__dirname, "../../")
-    }],
-  ],
+    [
+      require("babel-plugin-dotenv-milkywire"),
+      {
+        replacedModuleName: "react-native-dotenv-milkywire",
+        configDir: path.resolve(__dirname, "../../")
+      }
+    ]
+  ]
 });
